dc3d2afa88ffee9a4b8e86a5d7f3b91973f5cbcb
Salaaat <camorka.zelena@seznam.cz>
2022-09-26 12:43:55 +0200

    Create mop-svetlo.py
    matematicka olympiada - p
    tmava zakouti

---
+++ mop-svetlo.py
@@ -0,0 +1,15 @@
+def go():
+	first_line = input()
+	n = None
+	n_list = []
+	m = None
+	m_list = []
+	was_space = False
+	numbers = list(range(10))
+	
+	for i in first_line:
+		if i == j for j in numbers:
+			print('eej')
+
+while 1:
+	go()
