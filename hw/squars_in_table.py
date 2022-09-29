fc6be8004b787fd15e333aa66849b0fdbcb7e528
Salaaat <camorka.zelena@seznam.cz>
2022-09-29 18:44:28 +0200

    pot_sqrs
    Searching for sequances of True (works)

--- hw/squars_in_table.py
+++ hw/squars_in_table.py
@@ -1,6 +1,6 @@
 from random import randint
 
-def mk_table(min, max):
+def mk_table(min, max):	#makes random table of Trues and Falses
 	n = randint(min, max)
 	m = randint(min, max)
 	table = []
@@ -15,24 +15,34 @@ def mk_table(min, max):
 
 	return table
 
-def find_sqr():
-	min = 4
-	max = 10
+def find_sqr(min, max):	#findes the biggest square of Trues in random table of booleans
+	#min = int(input('Minimum size of table: '))
+	#max = int(input('Maximum size of table: '))
 	table = mk_table(min, max)
-	pot_sqrs = [[]]
-	sqrs = [[[0,0],[0,0]]]
+	pot_sqrs = []	#index = length of one side; pot_sqrs[index] = list of coordinates of top left corners; i, j = table[i][j] (coordinates in table)
+	sqrs = [[0,0]]	#index = length of one side; i, j = table[i][j] (coordinates of top left corners)
 
-	for i in table:
+	for i in range(len(table)):
+		row = table[i]
 		length = 0
-		for j in i:
-			if j:
+		print(row)
+
+		for j in range(len(row)):
+			cell = row[j]
+			print(cell)
+
+			if cell:
 				length += 1
+				while (len(sqrs) -1) < length:
+					sqrs.append([])
+				while (len(pot_sqrs) -1) < length:
+					pot_sqrs.append([])
+				
+				not_len = length
+				while not_len:
+					if not sqrs[not_len]:
+						pot_sqrs[not_len].append([i, j -(not_len -1)])
+					not_len -= 1
 			else:
 				length = 0
-			if len(sqrs) -1 < length:	#pot error
-				sqrs.append([])
-			if len(pot_sqrs) -1 < length:	#pot error
-				pot_sqrs.append([])
-			if not sqrs[length]:
-	print(table, pot_sqrs)
-find_sqr()
+
