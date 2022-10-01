9aafc7ea5b694a0ae2d701ad45cc6a621e018981
Salaaat <camorka.zelena@seznam.cz>
2022-10-01 17:35:14 +0200

    solving some small errors

--- hw/squars_in_table.py
+++ hw/squars_in_table.py
@@ -1,48 +1,50 @@
 from random import randint
 
-def mk_table(min, max):	#makes random table of Trues and Falses
-	n = randint(min, max)
-	m = randint(min, max)
-	table = []
 
-	for i in range(n):
-		table.append([])
-		for j in range(m):
-			if randint(0, 1):
-				table[i].append(True)
-			else:
-				table[i].append(False)
+def mk_table(min_size, max_size):  # makes random table of booleans
+    n = randint(min_size, max_size)
+    m = randint(min_size, max_size)
+    table = []
 
-	return table
+    for i in range(n):
+        table.append([])
+        for j in range(m):
+            if randint(0, 1):
+                table[i].append(True)
+            else:
+                table[i].append(False)
 
-def find_sqr(min, max):	#findes the biggest square of Trues in random table of booleans
-	#min = int(input('Minimum size of table: '))
-	#max = int(input('Maximum size of table: '))
-	table = mk_table(min, max)
-	pot_sqrs = []	#index = length of one side; pot_sqrs[index] = list of coordinates of top left corners; i, j = table[i][j] (coordinates in table)
-	sqrs = [[0,0]]	#index = length of one side; i, j = table[i][j] (coordinates of top left corners)
+    return table
 
-	for i in range(len(table)):
-		row = table[i]
-		length = 0
-		print(row)
+def find_sqr(min_size, max_size):  # finds the biggest square of Trues in random table of booleans
+    # min_size = int(input('Minimum size of table: '))
+    # max_size = int(input('Maximum size of table: '))
+    table = mk_table(min_size, max_size)
+    pot_sqrs = []  # index = length of one side; pot_sqrs[index] = list of coordinates of top left corners
+    sqrs = [[0, 0]]  # index = length of one side; i, j = table[i][j] (coordinates of top left corners)
 
-		for j in range(len(row)):
-			cell = row[j]
-			print(cell)
+    for i in range(len(table)):
+        row = table[i]
+        length = 0
+        print(row)
 
-			if cell:
-				length += 1
-				while (len(sqrs) -1) < length:
-					sqrs.append([])
-				while (len(pot_sqrs) -1) < length:
-					pot_sqrs.append([])
-				
-				not_len = length
-				while not_len:
-					if not sqrs[not_len]:
-						pot_sqrs[not_len].append([i, j -(not_len -1)])
-					not_len -= 1
-			else:
-				length = 0
+        for j in range(len(row)):
+            cell = row[j]
+            print(cell)
 
+            if cell:
+                length += 1
+                while (len(sqrs) - 1) < length:
+                    sqrs.append([])
+                while (len(pot_sqrs) - 1) < length:
+                    pot_sqrs.append([])
+
+                not_len = length
+                while not_len:
+                    if not sqrs[not_len]:
+                        pot_sqrs[not_len].append([i, j - (not_len - 1)])
+                    not_len -= 1
+            else:
+                length = 0
+
+    print(table, '\n', pot_sqrs)
